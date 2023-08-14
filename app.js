85271dec464df8fd07160ecb843c508896984147
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-08-13 21:10:44 -0700

    javascript-assigmment 02

---
+++ app.js
@@ -0,0 +1,247 @@
+// ==================== JAVASCRIPT ASSIGNMENT 02  CHAPTER 6-9 ============== //
+
+//  QUESTION NO : 1  Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your browser
+
+// The Value of A is 10
+
+// var a = 10;
+
+// var result = (a);
+
+// var result = (++a)
+
+// now the value of a is = 11
+
+// console.log("The value of ++a is" , result);
+
+// var result = (a++)
+
+// now the value of a is 12 
+
+// console.log("the value of a++ is" , result )
+
+// var result = (--a)
+
+// now the value of a is 11
+
+// console.log("the valye of --a is", result)
+
+
+// var result = (a--);
+
+
+// now the value of a is 10 
+
+// console.log("the valye of a-- is" , result )
+
+// var result = (a);
+
+// console.log( "now the value of a is " , result)
+
+
+
+
+// QUESTION NO : 2  What will be the output in variables a, b & result after
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+
+// Explain the output at each stage:
+
+// --a; 1
+// --a - --b; 1 - 0
+// --a - --b + ++b; 1 - 0 + 1
+// --a - --b + ++b + b--; 1 - 0 + 1 + 1
+
+// Result of A and B is  =======   ( 3 )   
+
+
+// QUESTION NO 6 
+
+// 6. Take
+// a) Take three subjects name from user and store them in 3
+// different variables.
+// b) Total marks for each subject is 100, store it in another
+// variable.
+// c) Take obtained marks for first subject from user and
+// stored it in different variable.
+
+
+// let subject1 = prompt ("Enter the name of the first subject"," maths");
+// let subject2 = prompt ("Enter the name of the second subject", "physics");
+// let subject3 = prompt ("Enter the name of the third subject", "urdu");
+
+// console.log("Subject1" , + subject1);
+// console.log("Subject2" , + subject2);
+// console.log("Subject3" , + subject3);
+
+// var totalMarks = 100;
+// let obtainedmarksubject1 = prompt ("Enter the obtained marks for subject1 "," maths" ,+totalMarks);
+
+
+
+
+
+     // =========== CHAPTER 9 - 11 ======================//
+
+
+//               1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// var city = prompt ("If users enter Karachi then show message  Welcome To city of ligts");
+
+// if(city === karachi){
+//     alert("welcome to city of lights")
+// }
+
+
+
+//               2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt("enter ur gender", "Male")
+
+// var massage = "not found"
+
+// if(gender == "Male"){
+//     message = "Hello sir"
+// }if(gender == "Female"){
+//     message ="Hello madam"
+// };
+
+// alert(message)
+
+// console.log(gender)
+
+
+
+
+//              3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+// Signal color Message
+// Red Must Stop
+// Yellow Ready to move
+// Green Move now
+
+// var colors = prompt ("Enter Traffic signal color")
+
+// if(colors === "red"){
+//     message = "Must stop"
+// }if(colors === "yellow"){
+//     message = "Ready to move"
+// }if(colors === "green"){
+//     message = "move now"
+// }
+// alert(message);
+// console.log(colors)
+
+
+//  QUESTION 5  Run this script, & check whether alert message would be
+// displayed or not. Record the outputs check if condition is true and alert will must work
+
+
+// var a = 10;
+
+// b = (++a);
+
+// if(a === 11){
+//     alert ("Condition is true")
+// }
+
+
+
+// ============ CHAPTER 12-13 ================= //
+
+// ---------->  IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITION <--------------- //
+
+
+// 5. Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise.
+
+
+// var password = prompt ("Enter your password");
+// var correctpassword = "123456"
+
+
+// if(password === "123456"){
+//     alert("Correct password!")
+// }else if(password == "1"){
+//     alert("Incorrect password")
+// }else if (password === ""){
+//     alert("enter your password")
+// }
+
+
+// var hour = 13;
+
+// if(hour < 13){
+//     alert("good day")
+// }else {
+//     alert("good morning")
+// }
+
+
+
+// var lanakyahai  = prompt ("")
+
+// if(lanakyahai === "doodh"){
+//     alert("le ana")
+// }else if(lanakyahai === "dahi"){
+//     alert("Challo yarr le ana")
+// }else if(lanakyahai === "breads"){
+//     alert("acha bhai kch be mile le ana")
+// }else{
+//     alert("khali hath mat ana bs ")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
